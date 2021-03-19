68db6fad01bd043ba92a7c1c4a7f595d721d4549
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2021-03-20 07:39:42 +0900

    draw test

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,11 @@ var hello = 'Hello TypeScript';
 if (elem) {
     elem.innerHTML = hello;
 }
+
+// Canvas
+var canvas = <HTMLCanvasElement> document.getElementById("canvas");
+var ctx = canvas.getContext("2d")!;
+
+// Draw
+ctx.fillStyle = "Blue";
+ctx.fillRect(15, 10, 150, 100);
